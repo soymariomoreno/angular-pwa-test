a1c185bcb9eb97f3baf61d5528b63280197a327e
Mario Moreno <mario.moreno.a93@gmail.com>
2019-09-16 17:47:32 -0500

    add ignore key acccess

--- messaging.bash
+++ messaging.bash
@@ -1,5 +1,5 @@
-curl -X POST -H "Authorization: key=AAAAjxS-IB0:APA91bHF36bmW_DDzD37e9mXF5ZhJtf6iCtbDj9FFF7Wfi5vbS98T2fHDQzMxG9r7e6tqCpq_vtEqbR9bGY_xgJVeW2o4AkuhpDk0qUMFCKJXCueofv3slFhNKSNJtZJgfC8Pt7LcEAY" -H "Content-Type: application/json" -d '{
-  "to": "fMMJZ_5FrvQ:APA91bERMcVixscqYkHU2HN3eWcIwYM0AfvaFfOdBlHPCdypQYGRQBdeqF5vt11C5WZ6gfew1Q_Polqw-QKCIzBLEnWYD5DeAQhc8kLuRgHe32pDTQNrEwlnDsrVP6YYYXIzRwaMzo9L",
+curl -X POST -H "Authorization: key=" -H "Content-Type: application/json" -d '{
+  "to": "",
   "notification": {
     "title": "FCM Message Angular",
     "body": "This is an FCM Message",
