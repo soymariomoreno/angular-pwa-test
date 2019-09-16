90eb53e750810b93457c080e4cb5968306c8e22f
Mario Moreno <mario.moreno.a93@gmail.com>
2019-09-16 17:02:59 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyAOzu9gpRX_GJeJzgfswkydLgRuntk_TAA",
-    authDomain: "pwa-example-96211.firebaseapp.com",
-    databaseURL: "https://pwa-example-96211.firebaseio.com",
-    projectId: "pwa-example-96211",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "614528327709",
-    appId: "1:614528327709:web:9978169766ca58e0"
+    messagingSenderId: "",
+    appId: ""
   }
 };
